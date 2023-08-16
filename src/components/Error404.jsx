46d7077af04a61c41ae30267976803d7c9aade64
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-08-16 12:05:47 +0530

    Error404 page fixed

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,7 +1,6 @@
 export default function Error404(props) {
     return (
-        <div className={`mx-5 text-center position-absolute top-50 start-50 translate-middle text-${props.mode === 'dark' ? 'light' : 'dark'
-            } `}>
+        <div className={`mx-5 text-center position-absolute top-50 start-50 translate-middle text-${props.mode === 'dark' ? 'light' : 'dark'} `}>
             <h1>NOT FOUND (ERROR 404)</h1>
             <p className="fs-2">The page you are looking for does not exists.</p>
         </div>
